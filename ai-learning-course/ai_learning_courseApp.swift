b693bd00726b8257528ba4da114d5ae7a8abf07e
Александр Гужавин <a.guzhavin@alarmtrade.ru>
2025-12-01 16:45:55 +0300

    init proj

---
+++ ai-learning-course/ai_learning_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_learning_courseApp.swift
+//  ai-learning-course
+//
+//  Created by Александр on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ai_learning_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
